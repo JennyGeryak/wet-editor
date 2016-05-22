e312fc365eff0912065f1bda723f2d6172afe847
Ceditvodu <ceditvodu@gmail.com>
2016-05-22 17:47:35 +0300

    ended first type of deleting function

--- g_Module_Backspace.js
+++ g_Module_Backspace.js
@@ -41,12 +41,51 @@
 		}
 		else
 		{
+            // deleting a line and going to the previous
 			if(active_char.className.split(" ")[0] == 'wet-line-start')
 			{
 				var parent_s = active_char.parentNode;
-				console.log(parent_s);
-				parent_s.parentNode.removeChild(parent_s);
-				// !!!!!!!!!! change this.current_line 
+                var previous_line = parent_s.previousSibling;
+                
+				console.log(previous_line);
+                if(previous_line != null)
+                {
+                    parent_s.parentNode.removeChild(parent_s);
+                    // !!!!!!!!!! change this.current_line 
+                    // deleting enter pseudo sign
+                    options.object.current_line[options.index] = options.object.current_line[options.index] - 1;
+                    console.log(previous_line.childNodes[previous_line.childNodes.length-1]);
+                    previous_line.childNodes[previous_line.childNodes.length-1].className = previous_line.childNodes[previous_line.childNodes.length-1].className + ' ' + 'active';
+                    
+                    // getting active element that must be deleted
+                    var active_char = document.getElementsByClassName('active')[0];
+                    // getting previose element thet will be active after key pressed
+                    var previous_char = document.getElementsByClassName('active')[0].previousSibling;
+                    if(previous_char != null)
+                    {
+                        if(previous_char.className.split(' ')[0] == 'wet-line-start')
+                        {
+                            // getting original class name of the previous element
+                            var previous_char_original_class_name = document.getElementsByClassName('active')[0].previousSibling.className.trim();
+                            // deleting active element
+                            active_char.parentNode.removeChild(active_char);
+
+                            // making previous element to be an active 
+                            previous_char.className = previous_char_original_class_name + ' ' + 'active';	
+                        }
+                        else
+                        {
+
+                            // getting original class name of the previous element
+                            var previous_char_original_class_name = document.getElementsByClassName('active')[0].previousSibling.className.trim();
+                            // deleting active element
+                            active_char.parentNode.removeChild(active_char);
+                            // making previous element to be an active 
+                            previous_char.className = previous_char_original_class_name + ' ' + 'active';	
+                        }
+                    }
+                }
+                
 			}
 		}
 	}
