9f693ccf8bbc51c054e889383c0bc7bf27a49312
Ceditvodu <ceditvodu@gmail.com>
2016-05-28 20:33:51 +0300

    uglification

--- public/app/jdoc.js
+++ public/app/jdoc.js
@@ -1503,7 +1503,7 @@ var Divider = (function()
 		var divider = new Divider();
 		
 		// prepare previose element for next work
-		word = concrete_entity.getElementsByClassName('parent')[0];
+		var word = concrete_entity.getElementsByClassName('parent')[0];
 				
 		this.deletePrevioseCursor(concrete_entity);
 		
