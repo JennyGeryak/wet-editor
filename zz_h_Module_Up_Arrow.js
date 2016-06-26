11e8a92a4c73be3e6d18efda915d630b49f2cc4e
Ceditvodu <ceditvodu@gmail.com>
2016-06-26 17:48:01 +0300

    #45 finished this issue

--- zz_h_Module_Up_Arrow.js
+++ zz_h_Module_Up_Arrow.js
@@ -17,21 +17,25 @@ Module.getInstance().up_arrow = function(options)
   var divider = new Divider();
   var director = new Director(concrete_entity, "wet-", "active");
   var word = director.getParentWord();
-  var cursore_entity = director.getCursorEntity('active');
-  var after_cursore = director.getNextEntity(cursore_entity);
-  var before_cursore = director.getBeforeEntity(cursore_entity);
-  var cursore_parent = cursore_entity.parentNode;
+  var cursor_entity = director.getCursorEntity('active');
+  var after_cursor = director.getNextEntity(cursor_entity);
+  var before_cursor = director.getBeforeEntity(cursor_entity);
+  var cursor_parent = cursor_entity.parentNode;
   
-  var words_parent = word.parentNode || false;
-  var prefiose_line = director.getBeforeEntity(words_parent);
+  var line = word.parentNode || false;
+  var previose_line = director.getBeforeEntity(line);
   
   // if cursor on first line:
-  if(!prefiose_line)
+  if(!previose_line)
   {
-    console.log(words_parent);
+    
+  }
+  else
+  {
+    console.log(director.findCursorPosition(cursor_entity));
+    
   }
   
-  console.log(words_parent);
   // if cursor not on a first:
   
   
