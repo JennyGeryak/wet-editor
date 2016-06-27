44ea28d96514488574c029709b630b69884e9b1f
Ceditvodu <ceditvodu@gmail.com>
2016-06-27 16:14:45 +0300

    #47 wip

--- h_Director.js
+++ h_Director.js
@@ -685,7 +685,7 @@ var Director = (function()
 
   /**
     * @function setCursorOnPosition
-    * @desc give class according an element.
+    * @desc set cursor on some position on some line.
     * @param {Number} position - position on which will be paste cursor. 
     * @param {object} line - line in wich need to paste cursor.
     * @mamberof Director
@@ -693,7 +693,72 @@ var Director = (function()
     */
     this.setCursorOnPosition = function(position, line)
     { 
+      var parent_word = cursor.parentNode || false;
       
+      var character_count = 0;
+      
+      if(line)
+      {
+        var lines_elements = line.childNodes;
+        
+        // separating all characters
+        for(var i=0; i<lines_elements.length; i++)
+        {
+          if((lines_elements[i].className.split(' ').indexOf('parent') < 0)
+             &&(lines_elements[i].className.split(' ').indexOf('wet-word') >= 0))
+          {
+            lines_elements[i].innerHTML = this.divider.divide(lines_elements[i])
+          }
+        }
+        
+        // counting characters before cursor
+        for(var i=0; i<lines_elements.length; i++)
+        {
+          var stop = false;
+          
+          if(lines_elements[i].childNodes.length == 1)
+          {
+            character_count++;
+            
+            if(lines_elements[i].className.split(' ').indexOf('active') >= 0)
+            {
+              stop = true;
+              break;
+            }
+          }
+          else if(lines_elements[i].childNodes.length > 1)
+          {
+            var lines_elements_elements = lines_elements[i].childNodes;
+            
+            for(var j=0; j<lines_elements_elements.length; j++)
+            {
+              character_count++;
+              
+              console.log(lines_elements_elements[j])
+              
+              if(lines_elements_elements[j].className.split(' ').indexOf('active') >= 0)
+              {
+                stop = true;
+                break;
+              }
+            }
+          }
+          
+          if(stop)
+          {
+            break;
+          }
+        }
+        
+        // deseparating characters before cursor
+        for(var i=0; i<lines_elements.length; i++)
+        {
+          if(lines_elements[i].className.split(' ').indexOf('parent') < 0)
+          {
+            lines_elements[i].innerHTML = this.divider.concat(lines_elements[i]);
+          }
+        }
+      }      
     }
     
 //////////////////
